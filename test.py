d8735480868d01d88355ee6311c36bfe9e64be00
Gamika Punsisi <121822722+gamikapunsisi@users.noreply.github.com>
2024-05-15 09:48:08 +0530

    update

--- test.py
+++ test.py
@@ -9,25 +9,6 @@ app = Flask(__name__)
 def index():
      return send_file('gpt.html')
 
-# Define a route to handle incoming chat requests
-# @app.route('/chat', methods=['POST'])
-# def chat():
-#     # Initialize the Groq client
-#     # client = Groq(api_key=os.environ.get("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"))
-#     client = Groq(api_key="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")
-
-    
-#     # Get the user message from the request
-#     data = request.get_json()
-#     messages = data.get('messages', [])
-    
-#     # Create a chat completion using Groq API
-#     chat_completion = client.chat.completions.create(messages=messages, model="mixtral-8x7b-32768")
-    
-#     # Return the response to the frontend
-#     return jsonify(chat_completion.choices[0].message.content)
-
-
 @app.route('/chat', methods=['POST'])
 def chat():
     # Initialize the Groq client
